98f173e51311caafcb1c6cac41efdb2c1285593e
Ryan Kraus <rmkraus@gmail.com>
2015-04-21 22:44:00 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='VarEvents',
     version='1.0.0',
     license='Apache License 2.0',
     url='http://automic.us/projects/pyisy',
+    download_url='https://github.com/automicus/varevents/tarball/1.0.0',
     author='Ryan Kraus',
     author_email='automicus@gmail.com',
     description='Python module to create variables that can '
     + 'raise custom events.',
-    packages=find_packages(),
+    packages=['VarEvents'],
     zip_safe=False,
     include_package_data=True,
     platforms='any',
     install_requires=[],
+    keywords=['event'],
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache License 2.0',
